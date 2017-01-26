34b18089c5ccdc975bfe23e5c4ace34a721bdcf7
Robert MacDonald <robertmacdonald18@gmail.com>
2017-01-26 17:49:18 -0500

    unfinished pong game.

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -39,13 +39,13 @@ function draw() {
 }
 
 function keyPressed() {
-    if (keyCode === UP_ARROW) {
-        s.dir(0, -1);
-    } else if (keyCode === DOWN_ARROW) {
-        s.dir(0, 1);
-    } else if (keyCode === RIGHT_ARROW) {
-        s.dir(1, 0);
-    } else if (keyCode === LEFT_ARROW) {
-        s.dir(-1, 0);
+    switch (keyCode){
+        case UP_ARROW: s.dir(0, -1);
+            break;
+        case DOWN_ARROW: s.dir(0, 1);
+            break;
+        case LEFT_ARROW: s.dir(-1, 0);
+            break;
+        case RIGHT_ARROW: s.dir(1,0);
     }
 }
